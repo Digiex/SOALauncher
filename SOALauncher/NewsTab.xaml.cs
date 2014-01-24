408a17da4053ca20c3a96fe13d93750129fd2b70
jessenic <jessenic@digiex.net>
2014-01-24 22:39:50 +0200

    News, Fonts, Saves

--- SOALauncher/NewsTab.xaml.cs
+++ SOALauncher/NewsTab.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QDFeedParser;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -10,17 +11,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
 
 namespace SOALauncher
 {
-	/// <summary>
-	/// Interaction logic for NewsTab.xaml
-	/// </summary>
-	public partial class NewsTab : UserControl
-	{
-		public NewsTab()
-		{
-			this.InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for NewsTab.xaml
+    /// </summary>
+    public partial class NewsTab : UserControl
+    {
+        public NewsTab()
+        {
+            this.InitializeComponent();
+            HttpFeedFactory fact = new HttpFeedFactory();
+            fact.BeginCreateFeed(new Uri("http://rss.indiedb.com/games/seed-of-andromeda/downloads/feed/rss.xml"), (cb) =>
+            {
+                feed = fact.EndCreateFeed(cb);
+                Dispatcher.BeginInvoke(
+                new Action<NewsTab>((sender) =>
+                {
+                    NewsList.ItemsSource = feed.Items;
+                }),
+                new object[] { this }
+                );
+            });
+        }
+        IFeed feed;
+    }
 }
---
+++ SOALauncher/SavesTab.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq;
+
+namespace SOALauncher
+{
+    /// <summary>
+    /// Interaction logic for SavesTab.xaml
+    /// </summary>
+    public partial class SavesTab : UserControl
+    {
+        public SavesTab()
+        {
+            this.InitializeComponent();
+            if (!Directory.Exists(MainWindow.AppData + "Versions"))
+            {
+                Directory.CreateDirectory(MainWindow.AppData + "Versions");
+            }
+            var dir2path = System.IO.Path.DirectorySeparatorChar + "Release" + System.IO.Path.DirectorySeparatorChar + "Data" + System.IO.Path.DirectorySeparatorChar + "Saves";
+            foreach (var dir in Directory.GetDirectories(MainWindow.AppData + "Versions"))
+            {
+                var itm = new TreeViewItem();
+                itm.Header = new DirectoryInfo(dir).Name;
+
+                if (!Directory.Exists(dir + dir2path))
+                {
+                    Directory.CreateDirectory(dir + dir2path);
+                }
+                foreach (var dir2 in Directory.GetDirectories(dir + dir2path))
+                {
+                    itm.Items.Add(new TreeViewItem()
+                    {
+                        Header = new DirectoryInfo(dir2).Name
+                    });
+                }
+                SaveTree.Items.Add(itm);
+            }
+        }
+    }
+}
